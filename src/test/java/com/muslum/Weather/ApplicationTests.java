b4edffe196ad4290919e8ad9fc26b7fd5083a42f
Muslum <sezginmuslum@gmail.com>
2017-11-29 15:55:18 +0300

    cav repo token

--- src/test/java/com/muslum/Weather/ApplicationTests.java
+++ src/test/java/com/muslum/Weather/ApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertThat(1,equalTo(1));
 	}
 
 }
